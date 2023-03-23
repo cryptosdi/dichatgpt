620e43737cb29014c6c7df87eef0ecdae2388d30
shiguojing <shiguojing@wejoydata.com>
2023-03-23 15:21:21 +0800

    add index

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
-
+import openai
+import os
 
 app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route("/api/chat")
+@app.route("/")
 def index():
+    return 'Hi, welcome~'
+
+@app.route("/chat", methods=['POST'])
+def chat():
     return 'Hi, dichatgpt!'
 
 if (__name__) == '__main__':
