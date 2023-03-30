2635068a697bf5da2a5ec9f970ef6e4eabe410b5
shiguojing <shiguojing@wejoydata.com>
2023-03-30 16:28:20 +0800

    add dev

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 import openai
+from dotenv import load_dotenv
 import os
+
+# 加载 .env 文件中的配置信息
+load_dotenv()
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
