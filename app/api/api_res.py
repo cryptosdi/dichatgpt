043df7335532cef7e9a316af3b5b664aa53f8493
shiguojing <shiguojing@wejoydata.com>
2023-04-03 20:45:11 +0800

    login

--- app/api/api_res.py
+++ app/api/api_res.py
@@ -4,5 +4,6 @@ class ApiRes:
     BAD_REQUEST = (400, 'bad_request')
     BAD_USER_NAME = (401, 'username not empty')
     BAD_USER_PASSWORD = (402, 'password not empty')
+    ERROR_USER_PASSWORD = (403, 'password error')
     NO_ACCESS = (410, 'forbidden')
     NOT_FOUND = (420, 'not_found')
--- app/api/login.py
+++ app/api/login.py
@@ -1,7 +1,22 @@
 from flask import Flask, request
 from app.api import lg
+from app.model.user import user
+from app.utils import jsonify_with_data
+from app.utils import jsonify_with_error
+from app.api.api_res import ApiRes
 
 
 @lg.route('', methods=['POST'])
 def login():
-    return 'login34567876543'
+    user_name = request.json.get('un').strip()
+    if len(user_name) == 0:
+       return jsonify_with_error(ApiRes.BAD_USER_NAME)  
+    password = request.json.get('pw').strip()
+    if len(password) == 0:
+       return jsonify_with_error(ApiRes.BAD_USER_PASSWORD)
+    usr = user.query(user_name)
+    if usr is None:
+        return jsonify_with_error(ApiRes.BAD_REQUEST) 
+    if usr.password != password:
+        return jsonify_with_error(ApiRes.ERROR_USER_PASSWORD)  
+    return jsonify_with_data(ApiRes.OK)
