cb2b22c79d83095dce1948a049ccc7c8b9384c0a
shiguojing <shiguojing@wejoydata.com>
2023-04-04 14:14:23 +0800

    add log

--- app/api/web.py
+++ app/api/web.py
@@ -6,11 +6,12 @@ from app.utils import jsonify_with_error
 from app.api.api_res import ApiRes
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-
+from app.utils import logger
 
 @bp.route("/")
 @jwt_required()
 def index():
     user_id = get_jwt_identity()
+    logger.info("[gpt] index user_id=%s", user_id)
     return jsonify_with_data(ApiRes.OK, user_id=user_id)
 
