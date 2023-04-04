771454e290e234090bf9a4a7828c73a7c46499d4
shiguojing <shiguojing@wejoydata.com>
2023-04-04 11:49:57 +0800

    opt code

--- app/api/login.py
+++ app/api/login.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from app.api import lg
+from app import jwt
 from app.model.user import user
 from app.utils import jsonify_with_data
 from app.utils import jsonify_with_error
@@ -8,6 +9,7 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import create_refresh_token
+from app.utils import generate_random_string
 
 @lg.route('', methods=['POST'])
 def login():
@@ -32,3 +34,25 @@ def refresh():
     identity = get_jwt_identity()
     access_token = create_access_token(identity=identity)
     return jsonify_with_data(ApiRes.OK, access_token=access_token)
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify_with_error(ApiRes.EXPIRED_TOKEN)
+
+
+@lg.route("/reg", methods=['POST'])
+def reg():
+    user_name = request.json.get('un').strip()
+    if len(user_name) == 0:
+       return jsonify_with_error(ApiRes.BAD_USER_NAME)  
+    password = request.json.get('pw').strip()
+    if len(password) == 0:
+       return jsonify_with_error(ApiRes.BAD_USER_PASSWORD)  
+    usr = user.query(user_name)
+    if usr is not None:
+        return jsonify_with_error(ApiRes.BAD_REQUEST) 
+    user_id = generate_random_string(6)
+    user.save(user_id, user_name, password)
+    access_token = create_access_token(identity=user_id)
+    refresh_token = create_refresh_token(identity=user_id)
+    return jsonify_with_data(ApiRes.OK, access_token=access_token, refresh_token=refresh_token)
--- app/api/web.py
+++ app/api/web.py
@@ -1,14 +1,12 @@
 from flask import Flask, request
 from app.api import bp
 from app.model.user import user
-from app.utils import generate_random_string
 from app.utils import jsonify_with_data
 from app.utils import jsonify_with_error
 from app.api.api_res import ApiRes
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import create_access_token
-from app import jwt
+
 
 @bp.route("/")
 @jwt_required()
@@ -16,23 +14,3 @@ def index():
     user_id = get_jwt_identity()
     return jsonify_with_data(ApiRes.OK, user_id=user_id)
 
-@jwt.expired_token_loader
-def expired_token_callback():
-    return jsonify_with_error(ApiRes.EXPIRED_TOKEN)
-
-
-@bp.route("/reg", methods=['POST'])
-def reg():
-    user_name = request.json.get('un').strip()
-    if len(user_name) == 0:
-       return jsonify_with_error(ApiRes.BAD_USER_NAME)  
-    password = request.json.get('pw').strip()
-    if len(password) == 0:
-       return jsonify_with_error(ApiRes.BAD_USER_PASSWORD)  
-    usr = user.query(user_name)
-    if usr is not None:
-        return jsonify_with_error(ApiRes.BAD_REQUEST) 
-    user_id = generate_random_string(6)
-    user.save(user_id, user_name, password)
-    access_token = create_access_token(identity=user_id)
-    return jsonify_with_data(ApiRes.OK, access_token=access_token)
