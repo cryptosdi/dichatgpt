32545f7f78929d7be1d32b0a1fb1c8b88ab26c16
cryptodi <cryptodi2020@gmail.com>
2023-04-04 07:52:21 +0000

    opt code

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 
 import os
 import sqlalchemy as sa
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config(object):
     user = os.getenv('DB_ACCOUNT')
     password = os.getenv('DB_PASSWORD')
     database = os.getenv('DB_DATA_NAME')
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@127.0.0.1:3306/%s' % (
-    #    user, parPw, database)
     connection_url = sa.engine.URL.create(
         drivername="mysql+pymysql",
         username=user,
@@ -16,4 +17,4 @@ class Config(object):
     )
     SQLALCHEMY_DATABASE_URI = connection_url 
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
