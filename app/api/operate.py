82fcec0b215f2a01e079ea734dc819d326a7d507
shiguojing <shiguojing@wejoydata.com>
2023-04-07 18:54:36 +0800

    opt code

--- app/api/operate.py
+++ app/api/operate.py
@@ -18,13 +18,19 @@ def save(user_id, role, content, reply_content):
 
 
 def merge_history_message(user_id, role, content):
-    merge_his_messages = [{"role": role, "content": content}]
-    messages = message.query(user_id, 3)
-    if messages is None:
+    with app.app_context():
+        merge_his_messages = [
+            {"role": "system", "content": "You are a helpful assistant."}]
+        messages = message.query(user_id, 3)
+        if messages is None:
+            merge_his_messages.append({"role": role, "content": content})
+            return merge_his_messages
+        for item in messages:
+            j_item = json.loads(item.message)[0]
+            merge_his_messages.insert(1,
+                                      {"role": j_item['role'], "content": j_item['content']})
+        merge_his_messages.append({"role": role, "content": content})
         return merge_his_messages
-    for item in messages:
-        merge_his_messages.append(json.loads(item.message))
-    return merge_his_messages
 
 
 def query_history_message(user_id, count):
