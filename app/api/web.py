1d143cbac400789bf62247bc3930a84785b7702d
shiguojing <shiguojing@wejoydata.com>
2023-06-16 15:17:05 +0800

    url

--- app/api/web.py
+++ app/api/web.py
@@ -1,22 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
 from app.api import bp
 from app.utils import jsonify_with_data
 from app.utils import jsonify_with_error
 from app.service import ApiRes
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-from app.utils import logger
 from app import limiter
-from flask import Flask, Response
 import time
 
 @bp.route("/")
 @jwt_required(optional=True)
 @limiter.limit("5/minute")
 def index():
-    user_id = get_jwt_identity()
-    #return Response(generate_text(), mimetype="text/plain")
-    return Response(generate_text(), mimetype='application/octet-stream')
+    #user_id = get_jwt_identity()
+    return url_for('home')
+    #return Response(generate_text(), mimetype='application/octet-stream')
 def generate_text():
     yield "Hello "
     time.sleep(1)
